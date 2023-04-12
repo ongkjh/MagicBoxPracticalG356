1298e06856a4b34d9b58f96a31a28a7f0a0dc9f6
anreh <anurehka@gmail.com>
2023-04-12 12:00:11 +0800

    First commit

---
+++ app/src/main/java/com/example/magicboxpracticalg356/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.magicboxpracticalg356
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
